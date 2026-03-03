8725af838dc356997904635783c2934a90ecb8b3
ComradePenguin-1917 <3069573702@qq.com>
2026-03-03 13:35:40 +0000

    Unify API for cuTile and Triton

--- tri_ins/__init__.py
+++
@@ -1,5 +0,0 @@
-from .triton_instrument import TritonInstrument, instrument_ptx
-from .ptx_parser import sub_block, builder
-from . import sanitizer
-
-__all__ = ['TritonInstrument', 'instrument_ptx', 'sub_block', 'builder', 'sanitizer']
